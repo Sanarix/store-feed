1bfba65a5f8d18c5ea824a97e2ede480ff976976
Leonid Korsakov <KorsakovLA@mail.ru>
2022-09-21 19:13:09 +0300

    Заготовка для появления кнопки применения фильтров

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ import cart from '../modules/cart.js';
 import Card from '../modules/Card.js';
 
 cart();
+
+let accordion = document.querySelector('.accordion');
+
+
+let lists = document.querySelectorAll('.checkbox-list');
+
+accordion.addEventListener('click', () => {
+	for(let list of lists) {
+		for(let li of list.children) {
+			for(let input of li.children) {
+				if(input.checked) {
+					document.querySelector('.aside-container').classList.remove('hide');
+					return
+				}else {
+					document.querySelector('.aside-container').classList.add('hide');
+				}
+			}
+		}
+	}
+})
